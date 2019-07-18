555e06b4e1a2d316e2272a938d9f3b211b1d3a04
Caio Rodrigues <caio.rm1@gmail.com>
2019-07-18 17:28:56 -0300

    update

--- tests/server.py
+++ tests/server.py
@@ -5,8 +5,15 @@ import shutil
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from .routes import routes
+import requests
 
-#https://gist.github.com/tliron/8e9757180506f25e46d9
+# https://gist.github.com/tliron/8e9757180506f25e46d9
+
+
+next_payload = None
+next_status_code = -1
+
+
 class TestHTTPServer(BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         self.routes = routes
@@ -34,74 +41,105 @@ class TestHTTPServer(BaseHTTPRequestHandler):
         return None
 
     def get_payload(self):
-        payload_len = int(self.headers.getheader('content-length', 0))
+        payload_len = int(self.headers['content-length'])
         payload = self.rfile.read(payload_len)
+        print(payload)
         payload = json.loads(payload)
         return payload
 
-    def handle_method(self, method):
-        route = self.get_route()
-        if route is None:
-            self.send_response(404)
+    def check_for_next_response(self):
+        global next_status_code
+        global next_payload
+        if self.path == '/set-response':
+            payload = self.get_payload()
+            next_status_code = payload['status_code']
+            next_payload = payload['payload']
+            self.send_response(200)
             self.end_headers()
-            self.wfile.write(bytes('Route not found\n', 'utf-8'))
+            return True
+        elif self.path == '/clear-response':
+            next_status_code = -1
+            next_payload = None
+            self.send_response(200)
+            self.end_headers()
+            return True
+        return False
+
+    def handle_method(self, method):
+        global next_status_code
+        global next_payload
+        if self.check_for_next_response():
+            return
+        elif (next_status_code > -1) and (not next_payload is None):
+            self.send_response(next_status_code)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(next_payload), 'utf-8'))
         else:
-            if method == 'HEAD':
-                self.send_response(200)
-                if 'media_type' in route:
-                    self.send_header('Content-type', route['media_type'])
+            route = self.get_route()
+            if route is None:
+                self.send_response(404)
                 self.end_headers()
+                self.wfile.write(bytes('Route not found\n', 'utf-8'))
             else:
-                if 'file' in route:
-                    if method == 'GET':
-                        try:
-                            f = open(os.path.join(here, route['file']))
+                if method == 'HEAD':
+                    self.send_response(200)
+                    if 'media_type' in route:
+                        self.send_header('Content-type', route['media_type'])
+                    self.end_headers()
+                else:
+                    if 'file' in route:
+                        if method == 'GET':
                             try:
+                                f = open(os.path.join(here, route['file']))
+                                try:
+                                    self.send_response(200)
+                                    if 'media_type' in route:
+                                        self.send_header(
+                                            'Content-type', route['media_type'])
+                                    self.end_headers()
+                                    shutil.copyfileobj(f, self.wfile)
+                                finally:
+                                    f.close()
+                            except:
+                                self.send_response(404)
+                                self.end_headers()
+                                self.wfile.write(
+                                    bytes('File not found\n', 'utf-8')
+                                )
+                        else:
+                            self.send_response(405)
+                            self.end_headers()
+                            self.wfile.write(
+                                bytes('Only GET is supported\n', 'utf-8')
+                            )
+                    else:
+                        if method in route:
+                            content = route[method](self)
+                            if content is not None:
                                 self.send_response(200)
                                 if 'media_type' in route:
                                     self.send_header(
                                         'Content-type', route['media_type'])
                                 self.end_headers()
-                                shutil.copyfileobj(f, self.wfile)
-                            finally:
-                                f.close()
-                        except:
-                            self.send_response(404)
+                                if method != 'DELETE':
+                                    self.wfile.write(
+                                        bytes(json.dumps(content), 'utf-8'))
+                            else:
+                                self.send_response(404)
+                                self.end_headers()
+                                self.wfile.write(bytes('Not found\n', 'utf-8'))
+                        else:
+                            self.send_response(405)
                             self.end_headers()
                             self.wfile.write(
-                                bytes('File not found\n', 'utf-8')
-                            )
-                    else:
-                        self.send_response(405)
-                        self.end_headers()
-                        self.wfile.write(
-                            bytes('Only GET is supported\n', 'utf-8')
-                        )
-                else:
-                    if method in route:
-                        content = route[method](self)
-                        if content is not None:
-                            self.send_response(200)
-                            if 'media_type' in route:
-                                self.send_header(
-                                    'Content-type', route['media_type'])
-                            self.end_headers()
-                            if method != 'DELETE':
-                                self.wfile.write(
-                                    bytes(json.dumps(content), 'utf-8'))
-                        else:
-                            self.send_response(404)
-                            self.end_headers()
-                            self.wfile.write(bytes('Not found\n', 'utf-8'))
-                    else:
-                        self.send_response(405)
-                        self.end_headers()
-                        self.wfile.write(
-                            bytes(method + ' is not supported\n', 'utf-8'))
+                                bytes(method + ' is not supported\n', 'utf-8'))
+
+
+server_address = ('127.0.0.1', 2020)
 
 
 def start_server():
-    server_address = ('127.0.0.1', 2020)
     print('Starting server -> %s:%d' % server_address)
     httpd = HTTPServer(server_address, TestHTTPServer)
     try:
@@ -109,6 +147,27 @@ def start_server():
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+
+
+def set_next_response(status_code, payload):
+    try:
+        response = requests.post(
+            url='http://%s:%d/set-response' % server_address,
+            json={'status_code': status_code, 'payload': payload}
+        )
+        return response.status_code == 200
+    except:
+        pass
+
+
+def clear_next_response():
+    try:
+        response = requests.post(
+            url='http://%s:%d/clear-response' % server_address
+        )
+        return response.status_code == 200
+    except:
+        pass
 
 
 _process = None
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,7 @@
 import unittest
 from panamah_sdk.client import AdminClient, StreamClient
-from .server import start as start_test_server, stop as stop_test_server
+from .server import start as start_test_server, stop as stop_test_server, set_next_response, clear_next_response
+
 
 class TestClient(unittest.TestCase):
     @classmethod
@@ -11,17 +12,29 @@ class TestClient(unittest.TestCase):
     def tearDownClass(cls):
         stop_test_server()
 
+    def tearDown(self):
+        clear_next_response()
+
     def test_admin_client(self):
         """testing admin client"""
         client = AdminClient("auth")
+        #get
+        set_next_response(200, {'id': 1})
         response = client.get("/admin/assinantes")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.content, 'utf-8'), '{"id": 1}')
+        #post
+        set_next_response(201, {'id': 1})
+        response = client.post("/admin/assinantes", {'id': 1})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(str(response.content, 'utf-8'), '{"id": 1}')
 
     def test_stream_client(self):
         """testing stream client"""
         client = StreamClient("auth", "secret")
         response = client.post("/stream/data", {"s": 1})
         self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
